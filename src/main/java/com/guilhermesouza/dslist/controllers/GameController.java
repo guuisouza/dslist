0e2d814a64a3df9f345fc4a5f104983d1547f180
Guilherme Souza <thekingguiiz2003@gmail.com>
2025-05-19 17:44:42 -0300

    feat: implements the game controller to receive http requests

---
+++ src/main/java/com/guilhermesouza/dslist/controllers/GameController.java
@@ -0,0 +1,24 @@
+package com.guilhermesouza.dslist.controllers;
+
+import com.guilhermesouza.dslist.dto.GameMinDTO;
+import com.guilhermesouza.dslist.entities.Game;
+import com.guilhermesouza.dslist.services.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/games")
+public class GameController {
+
+    @Autowired
+    private GameService gameService;
+
+    @GetMapping
+    public List<GameMinDTO> findAll() {
+        return gameService.findAll();
+    }
+}
