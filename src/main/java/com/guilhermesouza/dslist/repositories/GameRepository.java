d4b150e3784f3da65b2e9bb17f7e280848fe327b
Guilherme Souza <thekingguiiz2003@gmail.com>
2025-05-20 17:08:00 -0300

    feat: implement custom sql query in GameRepository

--- src/main/java/com/guilhermesouza/dslist/repositories/GameRepository.java
+++ src/main/java/com/guilhermesouza/dslist/repositories/GameRepository.java
@@ -1,8 +1,22 @@
 package com.guilhermesouza.dslist.repositories;
 
 import com.guilhermesouza.dslist.entities.Game;
+import com.guilhermesouza.dslist.projections.GameMinProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface GameRepository extends JpaRepository<Game, Long> {
 
+    // when uses nativeQuery (SQL), that means the result should be an Interface (Projection in Spring)
+    @Query(nativeQuery = true, value = """
+		SELECT tb_game.id, tb_game.title, tb_game.game_year AS `year`, tb_game.img_url AS imgUrl,
+		tb_game.short_description AS shortDescription, tb_belonging.position
+		FROM tb_game
+		INNER JOIN tb_belonging ON tb_game.id = tb_belonging.game_id
+		WHERE tb_belonging.list_id = :listId
+		ORDER BY tb_belonging.position
+			""")
+    List<GameMinProjection> searchByList(Long listId);
 }
