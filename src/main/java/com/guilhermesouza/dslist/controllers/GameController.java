81f4381df3acfee6792dea08426518065d744a33
Guilherme Souza <thekingguiiz2003@gmail.com>
2025-05-20 16:28:55 -0300

    feat: implements findById method on service and controller request

--- src/main/java/com/guilhermesouza/dslist/controllers/GameController.java
+++ src/main/java/com/guilhermesouza/dslist/controllers/GameController.java
@@ -1,10 +1,12 @@
 package com.guilhermesouza.dslist.controllers;
 
+import com.guilhermesouza.dslist.dto.GameDTO;
 import com.guilhermesouza.dslist.dto.GameMinDTO;
 import com.guilhermesouza.dslist.entities.Game;
 import com.guilhermesouza.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +22,10 @@ public class GameController {
     @GetMapping
     public List<GameMinDTO> findAll() {
         return gameService.findAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findAll(@PathVariable Long id) {
+        return gameService.findById(id);
     }
 }
--- src/main/java/com/guilhermesouza/dslist/services/GameService.java
+++ src/main/java/com/guilhermesouza/dslist/services/GameService.java
@@ -1,12 +1,17 @@
 package com.guilhermesouza.dslist.services;
 
+import com.guilhermesouza.dslist.dto.GameDTO;
 import com.guilhermesouza.dslist.dto.GameMinDTO;
 import com.guilhermesouza.dslist.entities.Game;
 import com.guilhermesouza.dslist.repositories.GameRepository;
+import com.guilhermesouza.dslist.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.lang.module.ResolutionException;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class GameService {
@@ -14,6 +19,14 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        Game result = gameRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Game ID " + id + " not found"));
+        return new GameDTO(result);
+    }
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
         return result.stream().map(GameMinDTO::new).toList();
