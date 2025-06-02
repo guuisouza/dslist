ac269d4ee33918cea97badb5cc049bbaeadefeb5
Guilherme Souza <thekingguiiz2003@gmail.com>
2025-06-02 16:59:08 -0300

    feat: implements a special endpoint, a logic for repositioning games in the desired list

--- src/main/java/com/guilhermesouza/dslist/services/GameListService.java
+++ src/main/java/com/guilhermesouza/dslist/services/GameListService.java
@@ -5,6 +5,7 @@ import com.guilhermesouza.dslist.dto.GameListDTO;
 import com.guilhermesouza.dslist.dto.GameMinDTO;
 import com.guilhermesouza.dslist.entities.Game;
 import com.guilhermesouza.dslist.entities.GameList;
+import com.guilhermesouza.dslist.projections.GameMinProjection;
 import com.guilhermesouza.dslist.repositories.GameListRepository;
 import com.guilhermesouza.dslist.repositories.GameRepository;
 import com.guilhermesouza.dslist.services.exceptions.ResourceNotFoundException;
@@ -20,9 +21,27 @@ public class GameListService {
     @Autowired
     private GameListRepository gameListRepository;
 
+    @Autowired
+    private GameRepository gameRepository;
+
     @Transactional(readOnly = true)
     public List<GameListDTO> findAll() {
         List<GameList> result = gameListRepository.findAll();
         return result.stream().map(GameListDTO::new).toList();
     }
-}
+
+    @Transactional
+    public void move(Long listId, int sourceIndex, int destinationIndex) {
+        List<GameMinProjection> list = gameRepository.searchByList(listId);
+
+        GameMinProjection obj = list.remove(sourceIndex);
+        list.add(destinationIndex, obj);
+
+        int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+        int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+        
+        for (int i = min; i <= max; i++) {
+            gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+        }
+    }
+ }
